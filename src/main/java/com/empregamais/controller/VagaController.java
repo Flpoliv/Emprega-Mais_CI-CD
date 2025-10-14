e1e2615aaea3967ec906b03f0ac0274102a4cdfc
Flpoliv <felipe710.oliveira1@gmail.com>
2025-10-13 21:55:58 -0300

    Commit teste

--- src/main/java/com/empregamais/controller/VagaController.java
+++ src/main/java/com/empregamais/controller/VagaController.java
@@ -1,13 +1,22 @@
 package com.empregamais.controller;
 
-import com.empregamais.model.Vaga;
-import com.empregamais.repository.VagaRepository;
-import jakarta.validation.Valid;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.empregamais.model.Vaga;
+import com.empregamais.repository.VagaRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/vagas")
@@ -49,3 +58,4 @@ public class VagaController {
 
 
 }
+//Teste
